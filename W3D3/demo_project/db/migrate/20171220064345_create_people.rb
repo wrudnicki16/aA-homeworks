951158bd6511ed206d737a5dcced8138113cb6c6
Wyatt Rudnicki <wyattrudnicki@gmail.com>
2017-12-20 23:41:10 -0800

    Add rails homeworks, half done

---
+++ W3D3/demo_project/db/migrate/20171220064345_create_people.rb
@@ -0,0 +1,9 @@
+class CreatePeople < ActiveRecord::Migration[5.1]
+  def change
+    create_table :people do |t|
+      t.integer :name, null: false
+      t.integer :house_id, null: false
+      t.timestamps
+    end
+  end
+end
---
+++ W3D3/demo_project/db/migrate/20171220080302_house.rb
@@ -0,0 +1,4 @@
+class House < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
